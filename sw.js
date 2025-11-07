539e7d3f872d8cac762cb00522c9e283d17b5af2
alterrion-git <blackrivercz@gmail.com>
2025-11-07 14:08:49 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker with Cache Versioning
-const CACHE_VERSION = 'v2.6.3'; // UPDATE THIS WITH EACH NEW VERSION!
+const CACHE_VERSION = 'v2.6.4'; // UPDATE THIS WITH EACH NEW VERSION!
 const CACHE_NAME = `workout-tracker-${CACHE_VERSION}`;
 
 const urlsToCache = [
